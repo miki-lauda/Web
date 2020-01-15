b8df70c8b8ea1b8d5028b11d4fcb485d7f327370
PC <PC@DESKTOP-A32UK90>
2020-01-15 22:08:20 +0100

    .

--- WebProject/src/rest/KategorijeServis.java
+++ WebProject/src/rest/KategorijeServis.java
@@ -1,16 +1,27 @@
 package rest;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 import com.google.gson.Gson;
 
 import beans.CloudService;
+import beans.KategorijaVM;
 
 public class KategorijeServis {
 
 	public static void loadService(CloudService cloud, Gson g) {
 		get("/Kategorije/getalljsonKategorije", (req, res) -> {
 			return g.toJson(cloud.getKategorije().values());
+		});
+		
+		post("/Kategorije/getKategoriju", (req, res) -> {
+			return g.toJson(cloud.getKategorije().values());
+		});
+		post("Kategorije/dodajKategoriju",(req,res)->{
+			KategorijaVM k=g.fromJson(req.body(), KategorijaVM.class);
+			cloud.getKategorije().put(k.getIme(), k);
+			return true;
 		});
 	}
 }
